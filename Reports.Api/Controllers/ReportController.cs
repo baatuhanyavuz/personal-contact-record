b04374cff5a870a470ce185db51f5100486549d4
Batuhan Yavuz <yavuz_batuhan97@hotmail.com>
2023-12-17 01:05:51 +0300

    rapor servisleri ve controller

---
+++ Reports.Api/Controllers/ReportController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Reports.Api.Dtos;
+using Reports.Api.Services.Abstract;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ReportController : ControllerBase
+{
+    private readonly IReportService _reportService;
+    public ReportController(IReportService reportService)
+    {
+
+        _reportService = reportService;
+    }
+
+    [HttpPost("ReportRequest")]
+    public async Task<ActionResult> ReportRequest(ReportRequestDto data)
+    {
+        await _reportService.RequestReportAsync(data);
+        return Ok();
+    }
+
+    [HttpGet("GetReportStatus")]
+    public async Task<ActionResult> GetReportStatus()
+    {
+        var result = await _reportService.GetReporStatus();
+        return Ok(result);
+    }
+
+
+}
---
+++ Reports.Api/Services/Concrete/ReportService.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Reports.Api.Context;
+using Reports.Api.Dtos;
+using Reports.Api.Entity;
+using Reports.Api.Services.Abstract;
+
+namespace Reports.Api.Services.Concrete
+{
+    public class ReportService : IReportService
+    {
+        private readonly AppDbContext _dbContext;
+      
+        public ReportService(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        
+        }
+
+        public async Task<Guid> RequestReportAsync(ReportRequestDto requestDto)
+        {
+            var report = new Report
+            {
+                ID = Guid.NewGuid(),
+                RequestedDate = DateTime.UtcNow,
+                ReportStatus = "Hazırlanıyor",
+                Location = requestDto.Location,
+            };
+
+            _dbContext.Reports.Add(report);
+            await _dbContext.SaveChangesAsync();
+
+            return report.ID;
+        }
+
+
+        public async Task<List<ReportStatusDto>> GetReporStatus()
+        {
+            var reportStatus = await _dbContext.Reports
+                 .Select(r => new ReportStatusDto
+                 {
+                     Id = r.ID,
+                     RequestAt = r.RequestedDate,
+                     Location = r.Location,
+                     Status = r.ReportStatus
+                 })
+                 .ToListAsync();
+
+            return reportStatus;
+        }
+    }
+}
