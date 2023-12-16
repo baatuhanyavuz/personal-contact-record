2e565669938d46baf174562e237ab5d31fbd1651
Batuhan Yavuz <yavuz_batuhan97@hotmail.com>
2023-12-17 02:02:57 +0300

    worker servis tamamlandi

--- PrepareReport.Api/Program.cs
+++ PrepareReport.Api/Program.cs
@@ -1,25 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using PrepareReport.Api.Context;
+using PrepareReport.Api.Services.Abstract;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")), ServiceLifetime.Singleton);
 
+builder.Services.AddScoped<RabbitMQService>();
+builder.Services.AddScoped<IWorkerService, WorkerService>();
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
-app.Run();
+using (var scope = app.Services.CreateScope())
+{
+    var workerService = scope.ServiceProvider.GetRequiredService<IWorkerService>();
+
+    while (true)
+    {
+        workerService.PrepareReport().Wait();
+    }
+}
--- PrepareReport.Api/Services/Abstract/IWorkerService.cs
+++ PrepareReport.Api/Services/Abstract/IWorkerService.cs
@@ -2,5 +2,6 @@
 {
     public interface IWorkerService
     {
+        Task PrepareReport();
     }
 }
