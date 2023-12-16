99d1275aab109715986508c4d9c2c0da82e1cf40
Batuhan Yavuz <yavuz_batuhan97@hotmail.com>
2023-12-16 22:05:43 +0300

    AddPerson controlleri tamamlandi

---
+++ Persons.Api/Controllers/PersonController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Persons.Api.Dtos;
+using Persons.Api.Services.Abstract;
+
+namespace Persons.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonsController : ControllerBase
+    {
+        private readonly IPersonService _personService;
+        public PersonsController(IPersonService personService)
+        {
+            _personService = personService;
+        }
+
+        [HttpPost("AddPerson")]
+        public async Task<ActionResult> AddPerson(PersonCreateDto data)
+        {
+            await _personService.AddPerson(data);
+            return Ok();
+        }
+    }
+}
