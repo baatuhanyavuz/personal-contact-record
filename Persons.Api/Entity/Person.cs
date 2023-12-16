0beaec3c70244acc0a24d8160b1b7938f284d6d8
Batuhan Yavuz <yavuz_batuhan97@hotmail.com>
2023-12-16 22:01:52 +0300

    entityler olusturuldu

---
+++ Persons.Api/Entity/Person.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Persons.Api.Entity
+{
+    public class Person
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid ID { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+
+        [MaxLength(100)]
+        public string Company { get; set; }
+
+        public virtual ICollection<ContactInformation> ContactInformation { get; set; }
+
+    }
+}
