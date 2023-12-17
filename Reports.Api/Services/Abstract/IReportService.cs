9029e87cc03e6423c62f7f94531b1b13f0d3d0a5
Batuhan Yavuz <yavuz_batuhan97@hotmail.com>
2023-12-17 16:54:35 +0300

    tum servisler ok. dockerfilelar ok. docker compose ok

--- Reports.Api/Services/Abstract/IReportService.cs
+++ Reports.Api/Services/Abstract/IReportService.cs
@@ -5,6 +5,7 @@ namespace Reports.Api.Services.Abstract
     public interface IReportService
     {
         Task<Guid> RequestReportAsync(ReportRequestDto requestDto);
+        Task<List<ReportDto>> GetReport();
         Task<List<ReportStatusDto>> GetReporStatus();
     }
 }
--- Reports.Api/Services/Concrete/ReportService.cs
+++ Reports.Api/Services/Concrete/ReportService.cs
@@ -30,10 +30,57 @@ namespace Reports.Api.Services.Concrete
             _dbContext.Reports.Add(report);
             await _dbContext.SaveChangesAsync();
 
-            _rabbitMQService.SendMessage(JsonConvert.SerializeObject(report));
+            _rabbitMQService.SendMessage((JsonConvert.SerializeObject(report)));
+
 
             return report.ID;
         }
+
+
+        public async Task<List<ReportDto>> GetReport()
+        {
+            var reports = await _dbContext.Reports
+                .Include(r => r.ReportDetails)
+                    .ThenInclude(rd => rd.Persons)
+                    .ThenInclude(p => p.ContactInformation)
+                .ToListAsync();
+
+            var reportDtos = reports
+                .Select(r => new ReportDto
+                {
+                    ID = r.ID,
+                    RequestedDate = r.RequestedDate,
+                    Location = r.Location,
+                    ReportStatus = r.ReportStatus,
+                    RegisteredPersonCount = r.RegisteredPersonCount,
+                    RegisteredPhoneNumberCount = r.RegisteredPhoneNumberCount,
+                    ReportDetails = r.ReportDetails.Select(rd => new ReportDetailDto
+                    {
+                        ID = rd.ID,
+                        ReportId = rd.ReportId,
+                        PersonId = rd.PersonId,
+                        Person = new PersonDto
+                        {
+                            ID = rd.Persons.ID,
+                            FirstName = rd.Persons.FirstName,
+                            LastName = rd.Persons.LastName,
+                            Company = rd.Persons.Company,
+                            ContactInformation = rd.Persons.ContactInformation
+                                .Select(ci => new ContactInformationDto
+                                {
+                                    Email = ci.Email,
+                                    Location = ci.Location,
+                                    Phone = ci.Phone
+                                }).ToList() ?? new List<ContactInformationDto>()
+                        }
+                    }).ToList()
+                })
+                .ToList();
+
+            return reportDtos;
+        }
+
+
 
         public async Task<List<ReportStatusDto>> GetReporStatus()
         {
